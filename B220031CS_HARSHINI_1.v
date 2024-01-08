1cc5bed7d0a37ae618ced9e333f1e1d9cabc7527
harshfromnow <133039634+harshfromnow@users.noreply.github.com>
2024-01-08 10:04:52 +0530

    Update and rename adder.v to B220031CS_HARSHINI_1.v

---
+++ B220031CS_HARSHINI_1.v
@@ -0,0 +1,18 @@
+//8 bit adder using behavioural modelling
+module adder(
+	input [7:0]a,
+	input [7:0]b,
+	output reg [7:0]sum,
+	output reg cout
+);
+
+	always@(a,b)
+	begin
+	sum=a+b;
+	if (a+b>255) cout=1'b1;
+	else cout=1'b0;
+	end
+endmodule
+
+//TESTBENCH
+
