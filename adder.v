e42ed5b275fc8501ed1a1ab4e8c6ffe32e027c99
harshfromnow <133039634+harshfromnow@users.noreply.github.com>
2024-01-08 09:56:13 +0530

    Add files via upload

---
+++ adder.v
@@ -0,0 +1,15 @@
+module adder(
+	input [7:0]a,
+	input [7:0]b,
+	output reg [7:0]sum,
+	output reg cout
+);
+
+	always@(a,b)
+	begin
+	sum=a+b;
+	if (a+b>255) cout=1'b1;
+	else cout=1'b0;
+	end
+endmodule
+	
