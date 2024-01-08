d1d23ece8f008b26fab8053ac6edac148c421dbb
harshfromnow <133039634+harshfromnow@users.noreply.github.com>
2024-01-08 09:57:40 +0530

    Update adder.v

--- adder.v
+++ adder.v
@@ -1,3 +1,4 @@
+//8 bit adder using behavioural modelling
 module adder(
 	input [7:0]a,
 	input [7:0]b,
@@ -12,4 +13,29 @@ module adder(
 	else cout=1'b0;
 	end
 endmodule
+
+//TESTBENCH
+
+module adder_tb;
+	reg [7:0]a;
+	reg [7:0]b;
+	wire [7:0]sum;
+	wire cout;
+	
+	adder uut(.a(a),.b(b),.sum(sum),.cout(cout));
+	integer i,j;
+	
+	 initial begin
+   	 for(i=0;i<256;i=i+1)
+      		begin
+		for (j=0;j<256;j=j+1)
+		 begin
+        		a=i;
+		  	b=j;
+        		#20;
+     		 end
+		end
+  	end
+  endmodule
+  
 	
