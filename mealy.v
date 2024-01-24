a62462bfa6590f734d9f483e0a94f88e44c0d7ed
harshfromnow <133039634+harshfromnow@users.noreply.github.com>
2024-01-24 18:35:13 +0530

    Update mealy.v

--- mealy.v
+++ mealy.v
@@ -1,31 +1,66 @@
 module mealy(
-  input clk,reset,inbits,
-  output reg out
-);
-  reg [1:0] state,next_state;
-  parameter S0 = 2'b00, S1 = 2'b01, S2 = 2'b10, S3 = 2'b11 ;
-  initial begin
-    state = S0;
-  end
+ input x,clk,reset,
+ output reg out
+ );
 
-  always@(posedge clk or posedge reset) 
-  	if (reset==0) state <= S0;
-	  else state<=next_state;
-    always @ (state, inbits) 
-    	case (state)
-    		S0:
-          if (inbits) next_state = S1; else next_state = S0;
-    		S1:
-          if (inbits) next_state = S1; else next_state = S2;
-        S2:
-          if (inbits) next_state = S3; else next_state = S0;
-    		S3:
-          if (inbits) next_state = S1; else next_state = S2;
-    	endcase
-always @ (state, x_in)
-	case (state)
-		S3:
-			out = 1;
-		S1, S2, S0: y_out = 0;
-	endcase
+ parameter S0 = 0 , S1 = 1 , S2 = 2;
+ reg [1:0] PS,NS ;
+
+ always @ (posedge clk or posedge reset)
+   if (reset)
+     PS <= S0;  
+   else
+     PS <= NS;
+   
+ always @ (posedge clk or posedge reset)
+   if (reset)
+     out <= 1'b0;
+   else
+     out <= (PS == S2) && x;
+  
+ always @ (*)
+  begin
+    case(PS)
+        S0 : NS = (x == 1) ? S1 : S0 ;
+        S1 : NS = (x == 0) ? S2 : S1;
+        S2 : NS = (x == 1) ? S1 : S0; 
+    endcase
+  end
+  
+ endmodule
+
+//TESTBENCH
+
+module mealy_tb;
+ // Inputs
+ reg x;
+ reg clk;
+ reg reset;
+ // Outputs
+ wire out;
+ // Instantiate the Unit Under Test (UUT)
+mealy uut (
+    .x(x), 
+    .clk(clk), 
+    .reset(reset), 
+    .out(out)
+);
+
+
+always #5 clk = ~ clk;  
+
+initial begin
+fork 
+    clk = 1'b0;
+    reset = 1'b1;
+    #15 reset = 1'b0;
+begin  
+    x = 0;#10 x = 1 ; #10 x = 0 ; #10 x = 1;
+    #10 x = 0;#10 x = 1 ; #10 x = 0 ; #10 x = 1 ;
+    #10 x = 1;#10 x = 0 ; #10 x = 0 ; #10 x = 1 ;
+    #10 x = 0;#10 x = 1 ; #10 x = 1 ; #10 x = 0 ;
+    #10 $finish; 
+end
+join   
+end  
 endmodule
